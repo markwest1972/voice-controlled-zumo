a31bf24df361f5e26695c5eafced1fbedead3552
markwest1972 <markwest1972@gmail.com>
2014-11-26 20:07:08 +0100

    Minor changes
    Associated with downloading and testing on a new machine

--- arduino_module/zumo-controller.js
+++ arduino_module/zumo-controller.js
@@ -6,52 +6,52 @@ board = new five.Board({
 
 board.on("ready", function() {
 
-  var speed = 80;
+  var speed = 0;
 
   motor1 = new five.Motor([10, 8]);
   motor2 = new five.Motor([9, 7]);
 
   board.repl.inject({
-  	lmotor: motor1,
+  	lmotor: motor1, 
   	rmotor: motor2,
   });
 
   require('net').createServer(function (socket) {
-     console.log("connected");
 
      socket.on('data', function (data) {
 
         switch ( data.toString()){
-        case 'faster':
-          speed+=20;
-          console.log(' => Up: ' + speed);
+        case 'engage':
+          speed=100;
           motor1.rev( speed );
           motor2.rev( speed );
           break;
-        case 'break':
+        case 'slower':
+          speed-=20;
+          motor1.rev( speed );
+          motor2.rev( speed );
+          break;
+        case 'faster':
           speed+=20;
-          console.log(' => Down: ' + speed);
-          motor1.fwd( speed );
-          motor2.fwd( speed );
+          motor1.rev( speed );
+          motor2.rev( speed );
           break;
         case 'turn left':
-          console.log(' => Left: ');
           motor1.fwd( speed * 0.5 );
           motor2.rev( speed * 0.5 );
           break;
         case 'turn right':
-          console.log('right');
           motor1.rev( speed * 0.5 );
           motor2.fwd( speed * 0.5 );
           break;
-        case 'stop':
-          console.log(' => Stopping...');
+        case 'disengage':
           motor1.stop();
           motor2.stop();
           break;
         case 'reset':
-          console.log(' => Speed to 80');
           speed = 80;
+          motor1.rev( speed );
+          motor2.rev( speed );
           break;
         default:
           console.log('Ignoring command: ' + data);
